daf1a36a0d82d957a27cfd5994fa17ecc26da05f
Claudio Ortolina <claudio@pspdfkit.com>
2020-07-02 11:17:05 +0100

    Remove duplicate name from homepage

--- test/tune_web/live/page_live_test.exs
+++ test/tune_web/live/page_live_test.exs
@@ -4,30 +4,30 @@ defmodule TuneWeb.PageLiveTest do
   import Phoenix.LiveViewTest
   import Mox
 
+  @avatar_url "http://example.com/user.png"
+  @profile %{
+    "display_name" => "Example user",
+    "images" => [
+      %{"url" => @avatar_url}
+    ]
+  }
+
+  @song_title "Example song"
+  @now_playing %{"item" => %{"name" => @song_title}}
+
   setup :verify_on_exit!
 
   test "disconnected and connected render", %{conn: conn} do
-    profile = %{
-      "display_name" => "Example user",
-      "images" => [
-        %{"url" => "http://example.com/user.png"}
-      ]
-    }
-
-    now_playing = %{"item" => %{"name" => "Example song"}}
-
     Tune.SpotifyMock
-    |> expect(:get_profile, 2, fn "example-token" -> {:ok, profile} end)
-    |> expect(:now_playing, 2, fn "example-token" -> {:playing, now_playing} end)
+    |> expect(:get_profile, 2, fn "example-token" -> {:ok, @profile} end)
+    |> expect(:now_playing, 2, fn "example-token" -> {:playing, @now_playing} end)
 
     {:ok, page_live, disconnected_html} =
       conn
       |> init_test_session(spotify_token: "example-token")
       |> live("/")
 
-    assert disconnected_html =~ "Example user"
-    assert disconnected_html =~ "Example song"
-    assert render(page_live) =~ "Example user"
-    assert render(page_live) =~ "Example song"
+    assert disconnected_html =~ @song_title
+    assert render(page_live) =~ @song_title
   end
 end
