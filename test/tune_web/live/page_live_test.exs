b1f1e858c65bd0d74eea135c3f4a72a07c609bbf
Claudio Ortolina <claudio@pspdfkit.com>
2020-07-04 11:06:31 +0100

    Display notice when logged out

--- test/tune_web/live/page_live_test.exs
+++ test/tune_web/live/page_live_test.exs
@@ -29,26 +29,71 @@ defmodule TuneWeb.PageLiveTest do
 
   setup :verify_on_exit!
 
-  test "disconnected and connected render", %{conn: conn} do
-    session_id = "example.user"
+  # Missing test cases:
+  #
+  # - Logged in, song pauses
+  # - Logged in, song resumes
+  # - Error with profile
+  # - Error with  now playing
 
-    credentials = %Ueberauth.Auth.Credentials{
+  describe "logged out" do
+    test "it displays not playing", %{conn: conn} do
+      {:ok, page_live, disconnected_html} = live(conn, "/")
+
+      login_required_message = "The application requires you to Login into your Spotify account."
+
+      assert disconnected_html =~ login_required_message
+      assert render(page_live) =~ login_required_message
+    end
+  end
+
+  describe "logged in" do
+    @session_id "example.user"
+    @credentials %Ueberauth.Auth.Credentials{
       token: "example-token",
       refresh_token: "refresh-token"
     }
 
-    Tune.SpotifyMock
-    |> expect(:setup, 2, fn ^session_id, ^credentials -> :ok end)
-    |> expect(:subscribe, 1, fn ^session_id -> :ok end)
-    |> expect(:get_profile, 2, fn ^session_id -> @profile end)
-    |> expect(:now_playing, 2, fn ^session_id -> {:playing, @now_playing} end)
+    setup %{conn: conn} do
+      Tune.SpotifyMock
+      |> expect(:setup, 2, fn @session_id, @credentials -> :ok end)
+      |> expect(:subscribe, 1, fn @session_id -> :ok end)
+      |> expect(:get_profile, 2, fn @session_id -> @profile end)
 
-    {:ok, page_live, disconnected_html} =
-      conn
-      |> init_test_session(spotify_id: session_id, spotify_credentials: credentials)
-      |> live("/")
+      [conn: init_test_session(conn, spotify_id: @session_id, spotify_credentials: @credentials)]
+    end
 
-    assert disconnected_html =~ @song_title
-    assert render(page_live) =~ @song_title
+    test "it displays not playing", %{conn: conn} do
+      Tune.SpotifyMock
+      |> expect(:now_playing, 2, fn @session_id -> :not_playing end)
+
+      {:ok, page_live, disconnected_html} = live(conn, "/")
+
+      assert disconnected_html =~ "Not playing."
+      assert render(page_live) =~ "Not playing."
+    end
+
+    test "it displays a song playing", %{conn: conn} do
+      Tune.SpotifyMock
+      |> expect(:now_playing, 2, fn @session_id -> {:playing, @now_playing} end)
+
+      {:ok, page_live, disconnected_html} = live(conn, "/")
+
+      assert disconnected_html =~ @song_title
+      assert render(page_live) =~ @song_title
+    end
+
+    test "it updates when the song changes", %{conn: conn} do
+      Tune.SpotifyMock
+      |> expect(:now_playing, 2, fn @session_id -> {:playing, @now_playing} end)
+
+      {:ok, page_live, _html} = live(conn, "/")
+
+      now_playing = {:playing, %{@now_playing | name: "Another song"}}
+
+      send(page_live.pid, now_playing)
+
+      render(page_live) =~ "Another song"
+    end
   end
 end
