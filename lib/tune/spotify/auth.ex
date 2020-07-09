89a27a5adb51074f0ec174c4964e49a229f354d7
Claudio Ortolina <claudio@pspdfkit.com>
2020-07-09 11:07:45 +0100

    Remove duplication in credential resolution

--- lib/tune/spotify/auth.ex
+++ lib/tune/spotify/auth.ex
@@ -1,7 +1,6 @@
 defmodule Tune.Spotify.Auth do
   def configure! do
-    client_id = System.get_env("SPOTIFY_CLIENT_ID")
-    client_secret = System.get_env("SPOTIFY_CLIENT_SECRET")
+    {client_id, client_secret} = get_credentials_from_env!()
 
     Application.put_env(:ueberauth, Ueberauth.Strategy.Spotify.OAuth,
       client_id: client_id,
@@ -10,9 +9,12 @@ defmodule Tune.Spotify.Auth do
   end
 
   def base64_encoded_credentials do
-    client_id = System.get_env("SPOTIFY_CLIENT_ID")
-    client_secret = System.get_env("SPOTIFY_CLIENT_SECRET")
+    {client_id, client_secret} = get_credentials_from_env!()
 
     Base.encode64(client_id <> ":" <> client_secret)
+  end
+
+  defp get_credentials_from_env! do
+    {System.get_env("SPOTIFY_CLIENT_ID"), System.get_env("SPOTIFY_CLIENT_SECRET")}
   end
 end
