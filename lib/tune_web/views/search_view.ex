a9fabd77d506943e5de9dde59524a904d0ebc54e
Claudio Ortolina <claudio@pspdfkit.com>
2020-07-10 19:22:51 +0100

    Resolve results thumbnail with preferred sizes

--- lib/tune_web/views/search_view.ex
+++ lib/tune_web/views/search_view.ex
@@ -6,19 +6,19 @@ defmodule TuneWeb.SearchView do
   alias Tune.Spotify.Schema.{Album, Artist, Episode, Show, Track}
 
   defp thumbnail(%Track{album: album}),
-    do: Map.get(album.thumbnails, :medium, @default_medium_thumbnail)
+    do: resolve_thumbnail(album.thumbnails, [:medium, :large])
 
   defp thumbnail(%Album{thumbnails: thumbnails}),
-    do: Map.get(thumbnails, :medium, @default_medium_thumbnail)
+    do: resolve_thumbnail(thumbnails, [:medium, :large])
 
   defp thumbnail(%Artist{thumbnails: thumbnails}),
-    do: Map.get(thumbnails, :medium, @default_medium_thumbnail)
+    do: resolve_thumbnail(thumbnails, [:medium, :large])
 
   defp thumbnail(%Show{thumbnails: thumbnails}),
-    do: Map.get(thumbnails, :medium, @default_medium_thumbnail)
+    do: resolve_thumbnail(thumbnails, [:medium, :large])
 
   defp thumbnail(%Episode{thumbnails: thumbnails}),
-    do: Map.get(thumbnails, :medium, @default_medium_thumbnail)
+    do: resolve_thumbnail(thumbnails, [:medium, :large])
 
   defp name(%Track{name: name}), do: name
   defp name(%Album{name: name}), do: name
@@ -31,4 +31,10 @@ defmodule TuneWeb.SearchView do
   defp author_name(%Show{publisher: publisher}), do: publisher.name
   defp author_name(%Artist{}), do: ""
   defp author_name(%Episode{}), do: ""
+
+  defp resolve_thumbnail(thumbnails, preferred_sizes) do
+    Enum.find_value(thumbnails, @default_medium_thumbnail, fn {size, url} ->
+      if size in preferred_sizes, do: url
+    end)
+  end
 end
