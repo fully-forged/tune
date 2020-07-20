1109d9c0fb9e86f84a5d32afa0188c8b9c65f659
Claudio Ortolina <claudio@pspdfkit.com>
2020-07-20 14:19:11 +0100

    Display total tracks and duration

--- lib/tune/spotify/schema/album.ex
+++ lib/tune/spotify/schema/album.ex
@@ -21,4 +21,26 @@ defmodule Tune.Spotify.Schema.Album do
     :thumbnails,
     :tracks
   ]
+
+  def total_duration_ms(album) do
+    case album.tracks do
+      :not_fetched ->
+        :not_available
+
+      tracks ->
+        Enum.reduce(tracks, 0, fn track, total_duration_ms ->
+          total_duration_ms + track.duration_ms
+        end)
+    end
+  end
+
+  def tracks_count(album) do
+    case album.tracks do
+      :not_fetched ->
+        :not_available
+
+      tracks ->
+        Enum.count(tracks)
+    end
+  end
 end
