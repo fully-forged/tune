4a05db995a417a1eabe4bd48ddc9155b4b449383
Claudio Ortolina <claudio@pspdfkit.com>
2020-07-23 17:24:21 +0100

    Typespec Session

--- lib/tune/spotify/session.ex
+++ lib/tune/spotify/session.ex
@@ -15,20 +15,20 @@ defmodule Tune.Spotify.Session do
   @type search_options :: [{:types, [item_type()]} | {:limit, pos_integer()}]
 
   @callback setup(id(), credentials()) :: :ok | {:error, term()}
-  @callback get_profile(id()) :: {:ok, %Schema.User{}} | {:error, term()}
+  @callback get_profile(id()) :: {:ok, Schema.User.t()} | {:error, term()}
   @callback now_playing(id()) ::
               :not_playing
-              | {:playing | :paused, %Schema.Track{} | %Tune.Spotify.Schema.Episode{}}
+              | {:playing | :paused, Schema.Track.t() | Schema.Episode.t()}
               | {:error, term()}
   @callback toggle_play(id()) :: :ok | {:error, term()}
   @callback play(id(), uri()) :: :ok | {:error, term()}
   @callback next(id()) :: :ok | {:error, term()}
   @callback prev(id()) :: :ok | {:error, term()}
   @callback search(id(), q(), search_options()) :: {:ok, [map()]} | {:error, term()}
-  @callback get_album(id(), item_id()) :: {:ok, %Schema.Album{}} | {:error, term()}
-  @callback get_artist(id(), item_id()) :: {:ok, %Schema.Artist{}} | {:error, term()}
-  @callback get_artist_albums(id(), item_id()) :: {:ok, [%Schema.Album{}]} | {:error, term()}
-  @callback get_show(id(), item_id()) :: {:ok, %Schema.Show{}} | {:error, term()}
+  @callback get_album(id(), item_id()) :: {:ok, Schema.Album.t()} | {:error, term()}
+  @callback get_artist(id(), item_id()) :: {:ok, Schema.Artist.t()} | {:error, term()}
+  @callback get_artist_albums(id(), item_id()) :: {:ok, [Schema.Album.t()]} | {:error, term()}
+  @callback get_show(id(), item_id()) :: {:ok, Schema.Show.t()} | {:error, term()}
 
   def subscribe(session_id) do
     PubSub.subscribe(Tune.PubSub, session_id)
