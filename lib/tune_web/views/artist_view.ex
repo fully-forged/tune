181d4392d1369aeb5141e149b91a4eb27c44136f
Claudio Ortolina <claudio@pspdfkit.com>
2020-07-24 08:38:04 +0100

    Typespec ArtistView

--- lib/tune_web/views/artist_view.ex
+++ lib/tune_web/views/artist_view.ex
@@ -1,13 +1,16 @@
 defmodule TuneWeb.ArtistView do
+  @moduledoc false
   use TuneWeb, :view
 
   @default_artwork "https://via.placeholder.com/300"
 
   alias Tune.Spotify.Schema.Artist
 
+  @spec artwork(Artist.t()) :: String.t()
   defp artwork(%Artist{thumbnails: thumbnails}),
     do: Map.get(thumbnails, :medium, @default_artwork)
 
+  @spec wikipedia_link(Artist.t()) :: String.t()
   defp wikipedia_link(artist) do
     Path.join([
       "https://en.wikipedia.org/wiki",
@@ -15,6 +18,7 @@ defmodule TuneWeb.ArtistView do
     ])
   end
 
+  @spec last_fm_link(Artist.t()) :: String.t()
   defp last_fm_link(artist) do
     Path.join([
       "https://www.last.fm/music",
