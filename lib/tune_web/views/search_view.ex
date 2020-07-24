cf04d131c01e01b194f7c98a50bd7b2c1082b6bf
Claudio Ortolina <claudio@pspdfkit.com>
2020-07-24 08:46:52 +0100

    Typespec SearchView

--- lib/tune_web/views/search_view.ex
+++ lib/tune_web/views/search_view.ex
@@ -1,10 +1,15 @@
 defmodule TuneWeb.SearchView do
+  @moduledoc false
   use TuneWeb, :view
 
   @default_medium_thumbnail "https://via.placeholder.com/150"
 
-  alias Tune.Spotify.Schema.{Album, Artist, Episode, Show, Track}
+  alias Tune.Spotify.Schema
+  alias Schema.{Album, Artist, Episode, Show, Track}
 
+  @type result_item() :: Album.t() | Artist.t() | Episode.t() | Show.t() | Track.t()
+
+  @spec thumbnail(result_item()) :: String.t()
   defp thumbnail(%Track{album: album}),
     do: resolve_thumbnail(album.thumbnails, [:medium, :large])
 
@@ -20,24 +25,28 @@ defmodule TuneWeb.SearchView do
   defp thumbnail(%Episode{thumbnails: thumbnails}),
     do: resolve_thumbnail(thumbnails, [:medium, :large])
 
+  @spec name(result_item()) :: String.t()
   defp name(%Track{name: name}), do: name
   defp name(%Album{name: name}), do: name
   defp name(%Show{name: name}), do: name
   defp name(%Artist{name: name}), do: name
   defp name(%Episode{name: name}), do: name
 
+  @spec author_name(result_item()) :: String.t()
   defp author_name(%Track{artist: artist}), do: artist.name
   defp author_name(%Album{artist: artist}), do: artist.name
   defp author_name(%Show{publisher: publisher}), do: publisher.name
   defp author_name(%Artist{}), do: ""
   defp author_name(%Episode{}), do: ""
 
+  @spec resolve_thumbnail(Schema.thumbnails(), [Schema.thumbnail_size()]) :: String.t()
   defp resolve_thumbnail(thumbnails, preferred_sizes) do
     Enum.find_value(thumbnails, @default_medium_thumbnail, fn {size, url} ->
       if size in preferred_sizes, do: url
     end)
   end
 
+  @spec result_link(result_item(), Phoenix.Socket.t()) :: String.t()
   def result_link(%Track{album: album}, socket) do
     Routes.explorer_path(socket, :album_details, album.id)
   end
