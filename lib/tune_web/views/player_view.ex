28f88f52201190e22b748e7bfcce938a24e66213
Claudio Ortolina <claudio@pspdfkit.com>
2020-08-01 17:01:13 +0100

    Extract mini player/progress bar to their own components
    This commit greatly reduces the amount of data sent over the wire due to
    changes in the current player.
    
    Before this commit, the entire explorer view would re-render anytime the
    currently playing item progress was updated (roughly every second).
    
    This caused a lot of bandwidth waste, as the only component concerned
    with progress updates is the progress bar included in the miniplayer.
    
    With this commit, the main ui is re-rendered only when either the
    playing item changes or playback is played/paused/stopped.
    
    Changes include:
    
    - The progress bar template fragment is promoted to a LiveComponent.
    - The ProgressBar component is rendered with an ID, which makes it
    stateful. As it's stateful, it's not removed and re-rendered when the
    parent changes.
    - Changes in the ProgressBar component are triggered by explicitly
    sending an update via 'send_update/2` from its parent ExplorerLive view.
    - When receiving now playing updates, the ExplorerLive component
    distinguishes between progress updates (which are sent to ProgressBar)
    and other updates, which re-render the rest of the UI.
    
    To extract markup containing the progress bar, it’s necessary to create
    a miniplayer component as it’s not possible to call a live component
    from a normal template.

--- lib/tune_web/views/player_view.ex
+++ lib/tune_web/views/player_view.ex
@@ -2,16 +2,7 @@ defmodule TuneWeb.PlayerView do
   @moduledoc false
   use TuneWeb, :view
 
-  @default_small_thumbnail "https://via.placeholder.com/48"
-
   alias Tune.Spotify.Schema.{Episode, Track}
-
-  @spec thumbnail(Track.t() | Episode.t()) :: String.t()
-  defp thumbnail(%Track{album: album}),
-    do: Map.get(album.thumbnails, :small, @default_small_thumbnail)
-
-  defp thumbnail(%Episode{thumbnails: thumbnails}),
-    do: Map.get(thumbnails, :small, @default_small_thumbnail)
 
   @spec name(Track.t() | Episode.t()) :: String.t()
   defp name(%Episode{name: name}), do: name
@@ -27,16 +18,4 @@ defmodule TuneWeb.PlayerView do
   end
 
   defp author_link(%Episode{}, _socket), do: nil
-
-  defp devices_options([]), do: nil
-
-  defp devices_options(devices) do
-    active_device = Enum.find(devices, fn d -> d.is_active end)
-
-    devices
-    |> Enum.map(fn d ->
-      {d.name, d.id}
-    end)
-    |> options_for_select(active_device.id)
-  end
 end
