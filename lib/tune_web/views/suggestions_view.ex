55ae672a6edac514b68ee5e5dccef5e36234be46
Claudio Ortolina <cloud8421@gmail.com>
2020-08-02 14:26:12 +0100

    Restructure templates (#39)
    - Extract suggestions/index
    - Move suggestions/playlist to playlist/show
    - Move album/tracklist to track/list

--- lib/tune_web/views/suggestions_view.ex
+++ lib/tune_web/views/suggestions_view.ex
@@ -2,16 +2,5 @@ defmodule TuneWeb.SuggestionsView do
   @moduledoc false
   use TuneWeb, :view
 
-  @default_artwork "https://via.placeholder.com/300"
-
-  alias Tune.Spotify.Schema.Playlist
-  alias TuneWeb.SearchView
-
-  @spec artwork(Playlist.t()) :: String.t()
-  defp artwork(%Playlist{thumbnails: thumbnails}),
-    do: Map.get(thumbnails, :large, @default_artwork)
-
-  defp group_label("single"), do: "Singles"
-  defp group_label("album"), do: "Albums"
-  defp group_label(other), do: other
+  alias TuneWeb.PlaylistView
 end
