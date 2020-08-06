8954e917b271b7cbd97df4a3d778bc8ea8a26f42
Claudio Ortolina <claudio@pspdfkit.com>
2020-08-06 11:27:07 +0100

    Minimal error handling for SDK errors

--- assets/js/app.js
+++ assets/js/app.js
@@ -45,6 +45,15 @@ Hooks.AudioPlayer = {
     this.player.addListener("ready", () => {
       this.pushEvent("refresh_devices", {});
     });
+
+    this.player.addListener("initialization_error", console.error);
+    this.player.addListener("authentication_error", console.error);
+    this.player.addListener("account_error", console.error);
+    this.player.addListener("playback_error", console.error);
+
+    this.player.addListener("not_ready", ({ device_id }) => {
+      console.warn("Device ID has gone offline", device_id);
+    });
   },
   token() {
     return this.el.dataset.token;
