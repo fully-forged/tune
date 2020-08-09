c5bad5b49359d89c00a00e8846d314b8161cbf3d
Claudio Ortolina <claudio@pspdfkit.com>
2020-08-09 16:49:53 +0100

    Reduce number of iterations on CI

--- config/test.exs
+++ config/test.exs
@@ -13,4 +13,4 @@ config :tune,
 config :logger, level: :warn
 
 config :stream_data,
-  max_runs: if(System.get_env("CI"), do: 200, else: 50)
+  max_runs: if(System.get_env("CI"), do: 100, else: 50)
