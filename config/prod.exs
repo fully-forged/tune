5308af8d11c1e8507099852ee48910c6f256451b
Claudio Ortolina <claudio@pspdfkit.com>
2020-08-24 14:35:28 +0100

    Remove unused Sentry tags

--- config/prod.exs
+++ config/prod.exs
@@ -23,9 +23,6 @@ config :sentry,
   environment_name: :prod,
   enable_source_code_context: true,
   root_source_code_path: File.cwd!(),
-  tags: %{
-    env: "production"
-  },
   included_environments: [:prod]
 
 # ## SSL Support
