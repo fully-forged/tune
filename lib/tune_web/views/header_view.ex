b46e4c9e526a1eecf86338b42377932206bc28b3
Claudio Ortolina <claudio@pspdfkit.com>
2020-08-24 09:21:05 +0100

    When a user has no avatar, show a fallback image

--- lib/tune_web/views/header_view.ex
+++ lib/tune_web/views/header_view.ex
@@ -1,4 +1,15 @@
 defmodule TuneWeb.HeaderView do
   @moduledoc false
   use TuneWeb, :view
+
+  defp user_avatar(user) do
+    case user.avatar_url do
+      nil -> fallback_avatar(user.name)
+      url -> url
+    end
+  end
+
+  defp fallback_avatar(name) do
+    "https://via.placeholder.com/45?text=" <> String.first(name)
+  end
 end
