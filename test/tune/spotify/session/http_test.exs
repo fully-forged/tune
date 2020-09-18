5f27d83fbbbf0ee3e0e867e87f1688017850f339
Claudio Ortolina <claudio@pspdfkit.com>
2020-09-18 08:30:37 +0100

    Add tests for errors in initial auth

--- test/tune/spotify/session/http_test.exs
+++ test/tune/spotify/session/http_test.exs
@@ -15,7 +15,62 @@ defmodule Tune.Spotify.Session.HTTPTest do
 
   setup [:set_mox_global, :verify_on_exit!]
 
-  describe "initialization and authentication" do
+  describe "failed authentication" do
+    @tag capture_log: true
+    test "with an expired token, the process stops" do
+      credentials = pick(Generators.credentials())
+      session_id = pick(Generators.session_id())
+
+      token = credentials.token
+
+      Client.Mock
+      |> expect(:get_profile, 1, fn ^token -> {:error, :invalid_token} end)
+
+      Process.flag(:trap_exit, true)
+
+      assert {:ok, session_pid} =
+               HTTP.start_link(session_id, credentials, timeouts: @default_timeouts)
+
+      assert_receive {:EXIT, ^session_pid, :invalid_token}
+    end
+
+    test "with an expired token, the process refreshes the token and retries" do
+      old_credentials = pick(Generators.credentials())
+      new_credentials = pick(Generators.credentials())
+      session_id = pick(Generators.session_id())
+      profile = pick(Generators.profile())
+
+      expect_token_refresh(old_credentials, new_credentials)
+      expect_profile(new_credentials.token, profile)
+      expect_nothing_playing(new_credentials.token)
+      expect_no_devices(new_credentials.token)
+
+      assert {:ok, session_pid} =
+               HTTP.start_link(session_id, old_credentials, timeouts: @default_timeouts)
+
+      assert profile == HTTP.get_profile(session_id)
+    end
+
+    test "with a transient network error, the process retries" do
+      credentials = pick(Generators.credentials())
+      session_id = pick(Generators.session_id())
+      profile = pick(Generators.profile())
+
+      expect_profile_with_network_error(credentials.token)
+      expect_profile(credentials.token, profile)
+      expect_nothing_playing(credentials.token)
+      expect_no_devices(credentials.token)
+
+      assert {:ok, session_pid} =
+               HTTP.start_link(session_id, credentials, timeouts: @default_timeouts)
+
+      Process.sleep(@default_timeouts.retry)
+
+      assert profile == HTTP.get_profile(session_id)
+    end
+  end
+
+  describe "successful authentication" do
     setup do
       [
         credentials: pick(Generators.credentials()),
@@ -74,9 +129,23 @@ defmodule Tune.Spotify.Session.HTTPTest do
     end
   end
 
+  defp expect_token_refresh(old_credentials, new_credentials) do
+    old_token = old_credentials.token
+    refresh_token = old_credentials.refresh_token
+
+    Client.Mock
+    |> expect(:get_profile, 1, fn ^old_token -> {:error, :expired_token} end)
+    |> expect(:get_token, 1, fn ^refresh_token -> {:ok, new_credentials} end)
+  end
+
   defp expect_profile(token, profile) do
     Client.Mock
     |> expect(:get_profile, 1, fn ^token -> {:ok, profile} end)
+  end
+
+  defp expect_profile_with_network_error(token) do
+    Client.Mock
+    |> expect(:get_profile, 1, fn ^token -> {:error, :nxdomain} end)
   end
 
   defp expect_nothing_playing(token) do
