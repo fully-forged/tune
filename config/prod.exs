dc39443972552e56d9294146054dec1fadd43acc
Claudio Ortolina <claudio@pspdfkit.com>
2020-10-30 11:31:57 +0000

    Use custom SentryFinch client

--- config/prod.exs
+++ config/prod.exs
@@ -23,6 +23,7 @@ config :sentry,
   environment_name: :prod,
   enable_source_code_context: true,
   root_source_code_path: File.cwd!(),
+  client: Sentry.FinchClient,
   included_environments: [:prod]
 
 # ## SSL Support
---
+++ lib/sentry/finch_client.ex
@@ -0,0 +1,28 @@
+defmodule Sentry.FinchClient do
+  @behaviour Sentry.HTTPClient
+
+  def child_spec do
+    opts = [name: Sentry.Finch]
+
+    Supervisor.child_spec(
+      %{
+        id: __MODULE__,
+        start: {Finch, :start_link, [opts]},
+        type: :supervisor
+      },
+      []
+    )
+  end
+
+  def post(url, headers, body) do
+    request = Finch.build(:post, url, headers, body)
+
+    case Finch.request(request, Sentry.Finch) do
+      {:ok, response} ->
+        {:ok, response.status, response.headers, response.body}
+
+      error ->
+        error
+    end
+  end
+end
